6e0a856929e91e01add1bd4f2b8f9ab28dc52349
iignjatovic <igor.ignjatovic8@gmail.com>
2015-01-06 11:14:05 +0100

    podaci dodani u "data zavrsni"

---
+++ workspaceZavrsni/postavljanjeStimuli.m
@@ -0,0 +1,24 @@
+stimuli.class(1) = 1;
+stimuli.class(2) = 2;
+stimuli.class(3) = 3;
+stimuli.class(4) = 4;
+stimuli.class(5) = 5;
+
+stimuli.start(1) = 
+stimuli.stop(1) =
+stimuli.start(2) =
+stimuli.stop(2) =
+stimuli.start(3) =
+stimuli.stop(3) =
+stimuli.start(4) =
+stimuli.stop(4) =
+stimuli.start(5) =
+stimuli.stop(5) =
+
+stimuli.number = 5;
+
+stimuli.duration(1) = stimuli.stop(1) - stimuli.start(1);
+stimuli.duration(2) = stimuli.stop(2) - stimuli.start(2);
+stimuli.duration(3) = stimuli.stop(3) - stimuli.start(3);
+stimuli.duration(4) = stimuli.stop(4) - stimuli.start(4);
+stimuli.duration(5) = stimuli.stop(5) - stimuli.start(5);
