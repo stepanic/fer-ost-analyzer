125386789cb3c6afe6992c4fd3d8446c85c310d4
Matija Stepanic <stepanic@loopus.co>
2015-01-15 18:07:18 +0100

    docs 7.2

--- workspaceZavrsni/facijalne2.m
+++ workspaceZavrsni/facijalne2.m
@@ -1,10 +1,10 @@
-b1 = rel_smooth(1:b1_i);
-n1 = rel_smooth(b1_i+1:n1_i);
-s = rel_smooth(n1_i+1:s_i);
-n2 = rel_smooth(s_i+1:n2_i);
-b2 = rel_smooth(n2_i+1:b2_i);
+b1 = oda_smooth(1:b1_i);
+n1 = oda_smooth(b1_i+1:n1_i);
+s = oda_smooth(n1_i+1:s_i);
+n2 = oda_smooth(s_i+1:n2_i);
+b2 = oda_smooth(n2_i+1:b2_i);
 
 len_control = length(b1) + length(b2) + length(n1) + length(n2) + length(s);
 
 
-data_rel = allDataGenerator(b1, n1, s, n2, b2); 
+data_oda = allDataGenerator(b1, n1, s, n2, b2); 
